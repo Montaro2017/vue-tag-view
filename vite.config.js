b20a68cb0f444a7e580652675a86de5c0cfe8716
Spark <kertlk@qq.com>
2025-07-19 16:47:50 +0800

    修改vite.config.js配置

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
 import UnoCss from "unocss/vite"
 import vue from '@vitejs/plugin-vue'
-import {defineConfig, loadEnv} from 'vite'
-import {createHtmlPlugin} from 'vite-plugin-html'
+import { defineConfig, loadEnv } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const root = process.cwd()
   const env = loadEnv(mode, root)
   const proxyServer = env.VITE_PROXY_SERVER
   const appTitle = env.VITE_APP_TITLE
   return {
+    root: 'vue-tag-view',
     plugins: [
       vue(),
       AutoImport({
